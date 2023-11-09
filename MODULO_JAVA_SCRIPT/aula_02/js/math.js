15a0c08eb67448eef9507022db69f3496668737c
Wesley Valério <wesley.enomoto@alunos.sc.senac.br>
2023-11-08 22:25:41 -0300

    adicionado aula_02

---
+++ MODULO_JAVA_SCRIPT/aula_02/js/math.js
@@ -0,0 +1,33 @@
+const soma = function(x, y){
+    return Number(x) + Number(y);
+    //return parseFloat(x) + parseFloat(y);
+    //return parseInt(x) + parseInt(y);
+    
+}
+
+const subtracao = function(x, y){
+    return x - y;
+}
+
+// criar a função multiplicação
+
+const multiplicacao = function(x, y){
+    return Number(x) * Number(y);
+}
+
+// função divisão
+
+const divisao = function(x, y){
+    return Number(x) / Number(y);
+}
+// função resto da divisão
+
+const restodivisao = function(x, y){
+    return Number(x) % Number(y);
+}
+
+// função potencia
+
+const potencia = function(x, y){
+    return Number(x) ** Number(y);
+}
